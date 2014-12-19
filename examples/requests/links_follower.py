87960113da5360a45e01aba076850d95d08cadc6
Javi <gatufo@gmail.com>
2014-12-19 01:02:25 +0100

    Added requests library example

---
+++ examples/requests/links_follower.py
@@ -0,0 +1,42 @@
+import re
+
+import requests
+
+from urlparse import urljoin
+
+from crawlfrontier.contrib.requests.manager import RequestsFrontierManager
+from crawlfrontier import Settings
+
+SETTINGS = Settings()
+SETTINGS.BACKEND = 'crawlfrontier.contrib.backends.memory.FIFO'
+SETTINGS.LOGGING_MANAGER_ENABLED = True
+SETTINGS.LOGGING_BACKEND_ENABLED = True
+SETTINGS.MAX_REQUESTS = 100
+SETTINGS.MAX_NEXT_REQUESTS = 10
+
+SEEDS = [
+    'http://www.imdb.com',
+]
+
+LINK_RE = re.compile(r'href="(.*?)"')
+
+
+def extract_page_links(response):
+    return [urljoin(response.url, link) for link in LINK_RE.findall(response.text)]
+
+if __name__ == '__main__':
+
+    frontier = RequestsFrontierManager(SETTINGS)
+    frontier.add_seeds([requests.Request(url=url) for url in SEEDS])
+    while True:
+        next_requests = frontier.get_next_requests()
+        if not next_requests:
+            break
+        for request in next_requests:
+                try:
+                    response = requests.get(request.url)
+                    links = [requests.Request(url=url) for url in extract_page_links(response)]
+                    frontier.page_crawled(response=response, links=links)
+                except requests.RequestException, e:
+                    error_code =  type(e).__name__
+                    frontier.request_error(request, error_code)
