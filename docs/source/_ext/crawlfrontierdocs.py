8a1942856c97a868f058d9d854b61a997fe1a9a6
Javi <gatufo@gmail.com>
2015-01-12 17:38:19 +0100

    Added source roles to docs

--- docs/source/_ext/crawlfrontierdocs.py
+++ docs/source/_ext/crawlfrontierdocs.py
@@ -1,6 +1,44 @@
+from docutils.parsers.rst.roles import set_classes
+from docutils import nodes
+
+
+REPO = 'https://github.com/scrapinghub/crawl-frontier/'
+
+
 def setup(app):
     app.add_crossref_type(
         directivename="setting",
         rolename="setting",
         indextemplate="pair: %s; setting",
     )
+    app.add_role('source', source_role)
+    app.add_role('commit', commit_role)
+    app.add_role('issue', issue_role)
+    app.add_role('rev', rev_role)
+
+
+def source_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    ref = REPO + 'blob/master/' + text
+    set_classes(options)
+    node = nodes.reference(rawtext, text, refuri=ref, **options)
+    return [node], []
+
+
+def issue_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    ref = REPO + 'issues/' + text
+    set_classes(options)
+    node = nodes.reference(rawtext, 'issue ' + text, refuri=ref, **options)
+    return [node], []
+
+
+def commit_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    ref = REPO + 'commit/' + text
+    set_classes(options)
+    node = nodes.reference(rawtext, text, refuri=ref, **options)
+    return [node], []
+
+def rev_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    ref = REPO + 'changeset/' + text
+    set_classes(options)
+    node = nodes.reference(rawtext, 'r' + text, refuri=ref, **options)
+    return [node], []
