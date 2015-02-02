ec997d4064fa6610f7677bb1ddf1f243f1589dd2
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-02-02 19:51:24 +0100

    Satisfying flask8.

--- crawlfrontier/contrib/backends/__init__.py
+++ crawlfrontier/contrib/backends/__init__.py
@@ -1,5 +1,6 @@
 from crawlfrontier.utils.url import urlparse_cached
 
+
 class OverusedBuffer(object):
     def __init__(self, _get_func, log_func=None):
         self._pending_requests = set()
@@ -13,20 +14,21 @@ class OverusedBuffer(object):
         requests = []
         _remove = []
         for request in self._pending_requests:
-            hostname = urlparse_cached(request).hostname or '' # FIXME: ip concurrency support
+            hostname = urlparse_cached(request).hostname or ''  # FIXME: ip concurrency support
             if hostname not in overused_keys:
                 requests.append(request)
                 _remove.append(request)
             if len(requests) == max_n_requests:
                 break
 
-        for r in _remove: self._pending_requests.remove(r)
+        for r in _remove:
+            self._pending_requests.remove(r)
 
         if len(requests) == max_n_requests:
             return requests
 
         for request in self._get(max_n_requests-len(requests), overused_keys):
-            hostname = urlparse_cached(request).hostname or '' # FIXME: ip concurrency support
+            hostname = urlparse_cached(request).hostname or ''  # FIXME: ip concurrency support
             if hostname in overused_keys:
                 self._pending_requests.add(request)
             else:
