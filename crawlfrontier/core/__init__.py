3a7fcb985edb42a3fb3c59a9035b2a50285b154c
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-02-03 17:29:23 +0100

    Optimization of O(N) scan in get_next_requests() with a more diverse collection of pending requests.

--- crawlfrontier/core/__init__.py
+++ crawlfrontier/core/__init__.py
@@ -1,4 +1,5 @@
 from urlparse import urlparse
+from collections import deque
 
 
 class OverusedKeys(list):
@@ -9,7 +10,7 @@ class OverusedKeys(list):
 
 class OverusedBuffer(object):
     def __init__(self, _get_func, log_func=None):
-        self._pending_requests = set()
+        self._pending = dict()
         self._get = _get_func
         self._log = log_func
 
@@ -19,22 +20,37 @@ class OverusedBuffer(object):
             raise NotImplementedError
         return key
 
+    def _get_pending(self, max_n_requests, overused_keys):
+        requests = []
+        trash_can = []
+        try:
+            while True:
+                left = 0
+                for key, pending in self._pending.iteritems():
+                    if key in overused_keys:
+                        continue
+
+                    if pending:
+                        requests.append(pending.popleft())
+                        if not pending:
+                            trash_can.append(key)
+                        left += len(pending)
+
+                    if len(requests) == max_n_requests:
+                        return requests
+
+                if left == 0:
+                    return requests
+        finally:
+            for k in trash_can:
+                del self._pending[k]
+
     def get_next_requests(self, max_n_requests, overused_keys):
         if self._log:
             self._log("Overused keys: %s" % str(overused_keys))
-            self._log("Pending: %i" % len(self._pending_requests))
-        requests = []
-        _remove = []
-        for request in self._pending_requests:
-            key = self._get_key(request, overused_keys.type)
-            if key not in overused_keys:
-                requests.append(request)
-                _remove.append(request)
-            if len(requests) == max_n_requests:
-                break
+            self._log("Pending: %i" % (sum([len(pending) for pending in self._pending.itervalues()])))
 
-        for r in _remove:
-            self._pending_requests.remove(r)
+        requests = self._get_pending(max_n_requests, overused_keys)
 
         if len(requests) == max_n_requests:
             return requests
@@ -42,7 +58,7 @@ class OverusedBuffer(object):
         for request in self._get(max_n_requests-len(requests), overused_keys):
             key = self._get_key(request, overused_keys.type)
             if key in overused_keys:
-                self._pending_requests.add(request)
+                self._pending.setdefault(key, deque()).append(request)
             else:
                 requests.append(request)
         return requests
