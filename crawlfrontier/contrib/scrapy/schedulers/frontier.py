dc56c97358d34e4e209f895f1f822c64f0234e5c
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-02-09 19:52:21 +0100

    Generalizing overused keys concept to DownloaderInfo. Plus documentation.

--- crawlfrontier/contrib/scrapy/schedulers/frontier.py
+++ crawlfrontier/contrib/scrapy/schedulers/frontier.py
@@ -5,7 +5,7 @@ from scrapy import log
 from collections import deque
 
 from crawlfrontier.contrib.scrapy.manager import ScrapyFrontierManager
-from crawlfrontier.core import OverusedKeys
+from crawlfrontier.core import DownloaderInfo
 
 STATS_PREFIX = 'crawlfrontier'
 
@@ -138,7 +138,7 @@ class CrawlFrontierScheduler(Scheduler):
     def _get_next_request(self):
         if not self.frontier.manager.finished and \
            not self.has_pending_requests():
-            for request in self.frontier.get_next_requests(overused_keys=self._get_overused_keys()):
+            for request in self.frontier.get_next_requests(downloader_info=self._get_downloader_info()):
                 self._add_pending_request(request)
         return self._get_pending_request()
 
@@ -157,11 +157,11 @@ class CrawlFrontierScheduler(Scheduler):
     def _request_is_redirected(self, request):
         return request.meta.get('redirect_times', 0) > 0
 
-    def _get_overused_keys(self):
+    def _get_downloader_info(self):
         downloader = self.crawler.engine.downloader
-        keys = OverusedKeys(type='ip' if downloader.ip_concurrency else 'domain')
+        info = DownloaderInfo(type='ip' if downloader.ip_concurrency else 'domain')
         for key, slot in downloader.slots.iteritems():
             overused_factor = len(slot.active) / float(slot.concurrency)
             if overused_factor > self.frontier.manager.settings.get('OVERUSED_SLOT_FACTOR', 1.0):
-                keys.append(key)
-        return keys
+                info.overused_keys.append(key)
+        return info
--- crawlfrontier/core/__init__.py
+++ crawlfrontier/core/__init__.py
@@ -1,24 +1,74 @@
 from urlparse import urlparse
+from socket import getaddrinfo
 from collections import deque
 
 
-class OverusedKeys(list):
+class DownloaderInfo(object):
+    """
+    Data class, used for carrying various information from downloading component of crawler to Crawl Frontier(CF)
+    backend. It's main purpose is to make backend aware of downloader fetching activities.
+
+    Typically the design of URL ordering implies fetching many URLs from the same domain. If crawling process needs to
+    be polite it has to preserve some delay and rate of requests. From the other side, there are downloaders which can
+    afford downloading many URLs at once, in parallel. So, flooding of the URLs from the same domain leads to
+    inefficient waste of downloader connection pool resources.
+
+    Using DownloaderInfo one can implement backend taking into account domain overuse in downloader and thus
+    returning URLs from other domains, which aren't overused.
+
+    The DownloaderInfo instance is created outside of Crawl Frontier, and then passed to CF via FrontierManagerWrapper
+    subclass to backend.
+    """
     def __init__(self, type='domain'):
-        super(OverusedKeys, self).__init__()
-        self.type = type
+        self._overused_keys = []
+        self._type = type
+
+    @property
+    def overused_keys(self):
+        """
+        Key here could be anything: domain, ip or custom object. It is defined in downloader and should be expected by
+        backend.
+
+        :return: A list of keys
+        """
+        return self._overused_keys
+
+    @property
+    def key_type(self):
+        return self._type
+
+
+def get_slot_key(request, type):  # TODO: Probably use caching here
+    """
+    Get string representing a downloader slot key, which will be used in downloader as id for domain/ip load
+    statistics and in backend for distinguishing free and overloaded resources.
+
+    :param request: :class:`Request <crawlfrontier.core.models.Request>`
+    :param type: a string 'ip' or 'domain'
+    :return: string
+    """
+    key = urlparse(request.url).hostname or ''
+    if type == 'ip':
+        for result in getaddrinfo(key, 80):
+            key = result[4][0]
+            break
+    return key
 
 
 class OverusedBuffer(object):
+    """
+    A buffering object for implementing the buffer of Crawl Frontier requests for overused domains/ips. It can be used
+    when customizing backend to address efficient downloader pool usage.
+
+    Attention! Scrapy users should use :class:`OverusedBufferScrapy <crawlfrontier.contrib.scrapy.overusedbuffer.OverusedBufferScrapy>`
+    """
     def __init__(self, _get_func, log_func=None):
         self._pending = dict()
         self._get = _get_func
         self._log = log_func
 
     def _get_key(self, request, type):
-        key = urlparse(request.url).hostname or ''
-        if type == 'ip':
-            raise NotImplementedError
-        return key
+        return get_slot_key(request, type)
 
     def _get_pending(self, max_n_requests, overused_set):
         requests = []
@@ -45,19 +95,19 @@ class OverusedBuffer(object):
             for k in trash_can:
                 del self._pending[k]
 
-    def get_next_requests(self, max_n_requests, overused_keys):
+    def get_next_requests(self, max_n_requests, downloader_info):
         if self._log:
-            self._log("Overused keys: %s" % str(overused_keys))
+            self._log("Overused keys: %s" % str(downloader_info.overused_keys))
             self._log("Pending: %i" % (sum([len(pending) for pending in self._pending.itervalues()])))
 
-        overused_set = set(overused_keys)
+        overused_set = set(downloader_info.overused_keys)
         requests = self._get_pending(max_n_requests, overused_set)
 
         if len(requests) == max_n_requests:
             return requests
 
-        for request in self._get(max_n_requests-len(requests), overused_keys):
-            key = self._get_key(request, overused_keys.type)
+        for request in self._get(max_n_requests-len(requests), downloader_info=downloader_info):
+            key = self._get_key(request, downloader_info.key_type)
             if key in overused_set:
                 self._pending.setdefault(key, deque()).append(request)
             else:
