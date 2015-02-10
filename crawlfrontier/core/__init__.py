fc98525e42626af091a5ae49e82dedb5c89dafc0
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-02-10 11:27:56 +0100

    Documentation.

--- crawlfrontier/core/__init__.py
+++ crawlfrontier/core/__init__.py
@@ -5,19 +5,10 @@ from collections import deque
 
 class DownloaderInfo(object):
     """
-    Data class, used for carrying various information from downloading component of crawler to Crawl Frontier(CF)
-    backend. It's main purpose is to make backend aware of downloader fetching activities.
+    Data class, used for carrying various information from downloading component of crawler to Crawl Frontier backend.
+    It's main purpose is to make backend aware of downloader fetching activities.
 
-    Typically the design of URL ordering implies fetching many URLs from the same domain. If crawling process needs to
-    be polite it has to preserve some delay and rate of requests. From the other side, there are downloaders which can
-    afford downloading many URLs at once, in parallel. So, flooding of the URLs from the same domain leads to
-    inefficient waste of downloader connection pool resources.
-
-    Using DownloaderInfo one can implement backend taking into account domain overuse in downloader and thus
-    returning URLs from other domains, which aren't overused.
-
-    The DownloaderInfo instance is created outside of Crawl Frontier, and then passed to CF via FrontierManagerWrapper
-    subclass to backend.
+    See :ref:`efficient parallel downloading <efficient-parallel-downloading>` best practices for more information.
     """
     def __init__(self, type='domain'):
         self._overused_keys = []
@@ -35,16 +26,20 @@ class DownloaderInfo(object):
 
     @property
     def key_type(self):
+        """
+        :return: string
+        """
         return self._type
 
 
 def get_slot_key(request, type):  # TODO: Probably use caching here
     """
     Get string representing a downloader slot key, which will be used in downloader as id for domain/ip load
-    statistics and in backend for distinguishing free and overloaded resources.
+    statistics and in backend for distinguishing free and overloaded resources. This method used in all Crawl
+    Frontier backends.
 
-    :param request: :class:`Request <crawlfrontier.core.models.Request>`
-    :param type: a string 'ip' or 'domain'
+    :param object request: is the instance of :class:`Request <crawlfrontier.core.models.Request>`.
+    :param str type: either 'domain'(default) or 'ip'.
     :return: string
     """
     key = urlparse(request.url).hostname or ''
@@ -59,10 +54,12 @@ class OverusedBuffer(object):
     """
     A buffering object for implementing the buffer of Crawl Frontier requests for overused domains/ips. It can be used
     when customizing backend to address efficient downloader pool usage.
-
-    Attention! Scrapy users should use :class:`OverusedBufferScrapy <crawlfrontier.contrib.scrapy.overusedbuffer.OverusedBufferScrapy>`
     """
     def __init__(self, _get_func, log_func=None):
+        """
+        :param _get_func: reference to get_next_requests() method of binded class
+        :param log_func: optional logging function, for logging of internal state
+        """
         self._pending = dict()
         self._get = _get_func
         self._log = log_func
--- crawlfrontier/core/components.py
+++ crawlfrontier/core/components.py
@@ -96,7 +96,8 @@ class Backend(Component):
         Returns a list of next requests to be crawled.
 
         :param int max_next_requests: Maximum number of requests to be returned by this method.
-        :param DownloaderInfo downloader_info: Information from downloader component, serving as hint for backend.
+        :param object downloader_info: Information from downloader component, serving as hint for backend. An instance \
+        of :class:`DownloaderInfo <crawlfrontier.core.DownloaderInfo>`
 
         :return: list of :class:`Request <crawlfrontier.core.models.Request>` objects.
         """
