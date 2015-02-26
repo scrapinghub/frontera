10ef7f04b3f8c6167e0943975d13051bcbf1fc73
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-02-26 19:18:27 +0100

    Distinguishing various message types.

--- crawlfrontier/worker/main.py
+++ crawlfrontier/worker/main.py
@@ -6,32 +6,55 @@ from crawlfrontier.settings import Settings
 
 from sys import stderr, argv
 
+
 class FrontierWorker(object):
-    def __init__(self, settings):
-        self.settings = Settings(module=settings)
-        self.consumer = KafkaConsumer(settings.get('INCOMING_TOPIC'),
-                                      group_id=settings.get('FRONTIER_GROUP'),
-                                      metadata_broker_list=[settings.get('KAFKA_LOCATION')])
+    def __init__(self, module_name):
+        self.settings = Settings(module=module_name)
+        self.consumer = KafkaConsumer(self.settings.get('INCOMING_TOPIC'),
+                                      group_id=self.settings.get('FRONTIER_GROUP'),
+                                      metadata_broker_list=[self.settings.get('KAFKA_LOCATION')])
         self.is_finishing = False
 
         self.decoder = ScrapyJSONDecoder()
-        self.manager = BaseManager.from_settings(settings)
+        self.manager = BaseManager.from_settings(self.settings)
         self.backend = self.manager.backend
+
+    def _response_from_object(self, obj):
+        request = self.manager._request_model(url=obj['url'],
+                                              meta=obj['meta'])
+        return self.manager._response_model(url=obj['url'],
+                     status_code=obj['status_code'],
+                     request=request)
+
+    def _request_from_object(self, obj):
+        return self.manager._request_model(url=obj['url'],
+                                           method=obj['method'],
+                                           headers=obj['headers'],
+                                           cookies=obj['cookies'],
+                                           meta=obj['meta'])
 
     def start(self):
         while not self.is_finishing:
             for m in self.consumer:
                 try:
-                    obj = self.decoder.decode(m.value)
+                    msg = self.decoder.decode(m.value)
                 except (KeyError, TypeError), e:
                     print >> stderr, "Decoding error: ", e
                     continue
                 else:
-                    response = self.manager._response_model(obj['response'])
-                    links = [self.manager._request_model(link) for link in obj['links']]
-                    self.backend.page_crawled(response, links)
+                    if msg['type'] == 'add_seeds':
+                        seeds = [self._request_from_object(seed) for seed in msg['seeds']]
+                        self.backend.add_seeds(seeds)
 
+                    if msg['type'] == 'page_crawled':
+                        response = self._response_from_object(msg['r'])
+                        links = [self.manager._request_model(link) for link in msg['links']]
+                        self.backend.page_crawled(response, links)
+
+                    if msg['type'] == 'request_error':
+                        request = self._request_from_object(msg['r'])
+                        self.backend.request_error(request, msg['error'])
 
 if __name__ == '__main__':
-    worker = FrontierWorker(argv[0])
+    worker = FrontierWorker(argv[1])
     worker.start()
