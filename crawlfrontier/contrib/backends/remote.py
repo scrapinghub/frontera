696be0a8fc9e8536a3c7c7ba865c14add3772c51
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-02-26 18:10:05 +0100

    Added meta to outgoing requests.

--- crawlfrontier/contrib/backends/remote.py
+++ crawlfrontier/contrib/backends/remote.py
@@ -5,7 +5,7 @@ from kafka.common import BrokerResponseError
 
 from scrapy.utils.serialize import ScrapyJSONEncoder, ScrapyJSONDecoder
 
-from crawlfrontier import Backend, Settings
+from crawlfrontier import Backend
 from crawlfrontier.core.models import Request
 
 
@@ -18,7 +18,6 @@ class TestManager(object):
             print "Test Manager: ", msg
 
         self.logger = TestManager.Nothing()
-        self.settings = Settings()
         self.logger.backend = TestManager.Nothing()
         for log_level in (
                 'info'
@@ -45,16 +44,29 @@ def prepare_response_message(response):
 
 
 class KafkaBackend(Backend):
-    def __init__(self, manager):
-        self._manager = manager
-        settings = manager.settings
+    DEFAULT_SERVER = "localhost:9092"
+    DEFAULT_GROUP = "scrapy-crawler"
+    DEFAULT_TOPIC_TODO = "frontier-todo"
+    DEFAULT_TOPIC_DONE = "frontier-done"
+    DEFAULT_WAIT_TIME = 5.0
+
+    def __init__(self, 
+                 manager=None,
+                 server=None, 
+                 group=None, 
+                 topic_todo=None, 
+                 topic_done=None, 
+                 wait_time=None):
+
+        self._manager = manager or TestManager()
+        self._seeds = []
 
         # Kafka connection parameters
-        self._server = settings.get('KAFKA_SERVER', "localhost:9092")
-        self._topic_todo = settings.get('KAFKA_TOPIC_TODO', "frontier-todo")
-        self._topic_done = settings.get('KAFKA_TOPIC_DONE', "frontier-done")
-        self._group = settings.get('KAFKA_GROUP', "scrapy-crawler")
-        self._get_timeout = float(settings.get('KAFKA_GET_TIMEOUT', 5.0))
+        self._server = server or KafkaBackend.DEFAULT_SERVER
+        self._topic_todo = topic_todo or KafkaBackend.DEFAULT_TOPIC_TODO
+        self._topic_done = topic_done or KafkaBackend.DEFAULT_TOPIC_DONE
+        self._group = group or KafkaBackend.DEFAULT_GROUP
+        self._wait_time = wait_time or KafkaBackend.DEFAULT_WAIT_TIME
 
         # Kafka setup
         self._conn = KafkaClient(self._server)
@@ -103,7 +115,13 @@ class KafkaBackend(Backend):
 
     @classmethod
     def from_manager(clas, manager):
-        return clas(manager)
+        return KafkaBackend(
+            manager=manager,
+            server=manager.settings.get('KAFKA_SERVER'),
+            group=manager.settings.get('KAFKA_GROUP'),
+            topic_todo=manager.settings.get('KAFKA_TOPIC_TODO'),
+            topic_done=manager.settings.get('KAFKA_TOPIC_DONE'),
+        )
 
     def frontier_start(self):
         if self._connect_consumer():
@@ -163,42 +181,43 @@ class KafkaBackend(Backend):
 
     def get_next_requests(self, max_n_requests):
         start = time.clock()
-        requests = []
+        if self._seeds:
+            n = min(len(self._seeds), max_n_requests)
+            requests, self._seeds = self._seeds[:n], self._seeds[n:]
+        else:
+            requests = []
 
-        if not self._connect_consumer():
-            return None
-
-        try:
-            success = False
-            for offmsg in self._cons.get_messages(
-                    max_n_requests,
-                    timeout=self._get_timeout):
-                success = True
-                try:
-                    obj = self._decoder.decode(offmsg.message.value)
+            if not self._connect_consumer():
+                return None
+            
+            try:
+                success = False
+                for offmsg in self._cons.get_messages(
+                        max_n_requests, 
+                        timeout=self._wait_time):
+                    success = True
                     try:
-                        requests.append(Request(url=obj['url'],
-                                                method=obj['method'],
-                                                headers=obj['headers'],
-                                                cookies=obj['cookies'],
-                                                meta=obj['meta']))
-                    except (KeyError, TypeError):
+                        obj = self._decoder.decode(offmsg.message.value)            
+                        try:
+                            requests.append(Request(url=obj['url'],
+                                                    meta=obj['meta']))
+                        except (KeyError, TypeError):
+                            self._manager.logger.backend.warning(
+                                "Could not get url field in message")
+                    except ValueError:
                         self._manager.logger.backend.warning(
-                            "Could not get url field in message")
-                except ValueError:
+                            "Could not decode {0} message: {1}".format(
+                                self._topic_todo,
+                                offmsg.message.value))
+                if not success:
                     self._manager.logger.backend.warning(
-                        "Could not decode {0} message: {1}".format(
-                            self._topic_todo,
-                            offmsg.message.value))
-            if not success:
+                        "Timeout ({0} seconds) while trying to get {1} requests".format(
+                            self._wait_time,
+                            max_n_requests)
+                    )
+            except BrokerResponseError:
                 self._manager.logger.backend.warning(
-                    "Timeout ({0} seconds) while trying to get {1} requests".format(
-                        self._get_timeout,
-                        max_n_requests)
-                )
-        except BrokerResponseError:
-            self._manager.logger.backend.warning(
-                "Could not connect consumer to " + self._topic_todo)
+                    "Could not connect consumer to " + self._topic_todo)
             
         self._manager.logger.backend.debug("get_next_requests: {0}".format(time.clock() - start))
         return requests
