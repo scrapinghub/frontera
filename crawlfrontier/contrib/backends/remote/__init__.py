56f86e13e5775f8221fc3876559af3eb4a75db73
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-03-04 11:11:49 +0100

    Using codecs in worker and KafkaBackend.

--- crawlfrontier/contrib/backends/remote/__init__.py
+++ crawlfrontier/contrib/backends/remote/__init__.py
@@ -1,1 +1,4 @@
 # -*- coding: utf-8 -*-
+from remote import KafkaBackend
+
+__all__ = ['KafkaBackend', 'KafkaJSONDecoder', 'KafkaJSONEncoder']
--- crawlfrontier/worker/main.py
+++ crawlfrontier/worker/main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from kafka import KafkaClient, KeyedProducer, SimpleConsumer
-from scrapy.utils.serialize import ScrapyJSONDecoder, ScrapyJSONEncoder
-from crawlfrontier.contrib.backends.remote import prepare_request_message
+from crawlfrontier.contrib.backends.remote.codecs import KafkaJSONDecoder, KafkaJSONEncoder
 from crawlfrontier.core.manager import BaseManager
 from crawlfrontier.settings import Settings
 
@@ -35,34 +34,24 @@ class FrontierWorker(object):
                                        max_buffer_size=1048576)
         self.is_finishing = False
 
-        self.decoder = ScrapyJSONDecoder()
-        self.encoder = ScrapyJSONEncoder()
+
         self.manager = BaseManager.from_settings(self.settings)
         self.backend = self.manager.backend
+        self.encoder = KafkaJSONEncoder(self.manager.request_model)
+        self.decoder = KafkaJSONDecoder(self.manager.request_model, self.manager.response_model)
+
 
         self.consumer_batch_size = self.settings.get('CONSUMER_BATCH_SIZE', 24)
         self.outgoing_topic = self.settings.get('OUTGOING_TOPIC')
 
-
-    def _response_from_object(self, obj):
-        request = self.manager._request_model(url=obj['url'],
-                                              meta=obj['meta'])
-        return self.manager._response_model(url=obj['url'],
-                     status_code=obj['status_code'],
-                     request=request)
-
-    def _request_from_object(self, obj):
-        return self.manager._request_model(url=obj['url'],
-                                           method=obj['method'],
-                                           headers=obj['headers'],
-                                           cookies=obj['cookies'],
-                                           meta=obj['meta'])
-
-    def _request_to_object(self, request):
-        return prepare_request_message(request)
-
     def start(self):
-        self.new_batch()
+        produced = self.new_batch()
+
+        if not produced:
+            logger.info("No requests were produced. Exiting.")
+            return
+
+        consumed = 0
         while not self.is_finishing:
             for m in self.consumer.get_messages(count=self.consumer_batch_size,
                                                 block=True,
@@ -73,37 +62,39 @@ class FrontierWorker(object):
                     logger.error("Decoding error: %s", e)
                     continue
                 else:
-                    if msg['type'] == 'add_seeds':
-                        logger.info('Adding seeds')
-                        seeds = []
-                        for seed in msg['seeds']:
-                            request = self._request_from_object(seed)
-                            seeds.append(request)
-                            logger.debug('URL: ', request.url)
-
+                    type = msg[0]
+                    if type == 'add_seeds':
+                        _, seeds = msg
+                        logger.info('Adding %i seeds', len(seeds))
+                        map(lambda seed: logger.debug('URL: ', seed.url), seeds)
                         self.backend.add_seeds(seeds)
 
-                    if msg['type'] == 'page_crawled':
-                        logger.info("Page crawled %s", msg['r']['url'])
-                        response = self._response_from_object(msg['r'])
-                        links = [self._request_from_object(link) for link in msg['links']]
+                    if type == 'page_crawled':
+                        _, response, links = msg
+                        logger.info("Page crawled %s", response.url)
                         self.backend.page_crawled(response, links)
 
-                    if msg['type'] == 'request_error':
-                        logger.info("Request error %s", msg['r']['url'])
-                        request = self._request_from_object(msg['r'])
-                        self.backend.request_error(request, msg['error'])
-            self.new_batch()
+                    if type == 'request_error':
+                        _, request, error = msg
+                        logger.info("Request error %s", request.url)
+                        self.backend.request_error(request, error)
+                finally:
+                    consumed += 1
+
+            logger.info("Consumed %d items.", consumed)
+            if consumed > produced * 0.7:
+                produced = self.new_batch()
+                consumed = 0
 
     def new_batch(self):
         count = 0
         for request in self.backend.get_next_requests(self.settings.MAX_NEXT_REQUESTS):
-            eo = self.encoder.encode(self._request_to_object(request))
-
+            eo = self.encoder.encode_request(request)
             # TODO: send in batches
             self.producer.send_messages(self.outgoing_topic, request.meta['domain']['fingerprint'], eo)
             count += 1
         logger.info("Pushed new batch of %d items", count)
+        return count
 
 
 if __name__ == '__main__':
