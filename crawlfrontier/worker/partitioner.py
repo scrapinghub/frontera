5444136e2bfef0878a18cfaa6ea5db01f8eee6d8
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-03-19 19:04:54 +0100

    Trying to get more even distribution of domain names by partitions.

--- crawlfrontier/worker/partitioner.py
+++ crawlfrontier/worker/partitioner.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from struct import unpack
+from binascii import unhexlify
 from kafka.partitioner.base import Partitioner
 
 
 class FingerprintPartitioner(Partitioner):
     def partition(self, key, partitions):
         size = len(partitions)
-        value = unpack("<I", key[:4])
+        digest = unhexlify(key[0:2] + key[5:7] + key[10:12] + key[15:17])
+        value = unpack("<I", digest)
         idx = value[0] % size
         return partitions[idx]
