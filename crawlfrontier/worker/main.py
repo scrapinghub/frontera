97b6a7f0d5421b912f8b96b4fe05cf91dd64d320
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-04-20 18:04:55 +0200

    Moving CallLaterOnce to utils.

--- crawlfrontier/worker/main.py
+++ crawlfrontier/worker/main.py
@@ -4,9 +4,7 @@ from argparse import ArgumentParser
 from time import asctime
 
 from twisted.internet import reactor
-from twisted.internet.defer import Deferred
-
-from kafka import KafkaClient, KeyedProducer, SimpleConsumer, KafkaConsumer
+from kafka import KafkaClient, KeyedProducer, SimpleConsumer
 from kafka.common import OffsetOutOfRangeError
 
 from crawlfrontier.contrib.backends.remote.codecs import KafkaJSONDecoder, KafkaJSONEncoder
@@ -15,7 +13,7 @@ from crawlfrontier.core.models import Request
 from crawlfrontier.settings import Settings
 from crawlfrontier.worker.partitioner import Crc32NamePartitioner
 from crawlfrontier.utils.url import parse_domain_from_url_fast
-
+from utils import CallLaterOnce
 from server import JsonRpcService
 from scorer import DiscoveryScorer
 from offsets import Fetcher
@@ -23,47 +21,6 @@ from offsets import Fetcher
 
 logging.basicConfig()
 logger = logging.getLogger("cf")
-
-
-class CallLaterOnce(object):
-    """Schedule a function to be called in the next reactor loop, but only if
-    it hasn't been already scheduled since the last time it run.
-    """
-    def __init__(self, func, *a, **kw):
-        self._func = func
-        self._a = a
-        self._kw = kw
-        self._call = None
-        self._errfunc = None
-        self._erra = None
-        self._errkw = None
-
-    def setErrback(self, func, *a, **kw):
-        self._errfunc = func
-        self._erra = a
-        self._errkw = kw
-
-    def schedule(self, delay=0.0):
-        if self._call is None:
-            d = Deferred()
-            d.addCallback(self)
-            if self._errfunc:
-                d.addErrback(self.error)
-            self._call = reactor.callLater(delay, d.callback, None)
-
-    def cancel(self):
-        if self._call:
-            self._call.cancel()
-
-    def __call__(self, *args, **kwargs):
-        self._call = None
-        return self._func(*self._a, **self._kw)
-
-    def error(self, f):
-        self._call = None
-        if self._errfunc:
-            return self._errfunc(f, *self._erra, **self._errkw)
-        return f
 
 
 class Slot(object):
---
+++ crawlfrontier/worker/utils.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+
+
+class CallLaterOnce(object):
+    """Schedule a function to be called in the next reactor loop, but only if
+    it hasn't been already scheduled since the last time it run.
+    """
+    def __init__(self, func, *a, **kw):
+        self._func = func
+        self._a = a
+        self._kw = kw
+        self._call = None
+        self._errfunc = None
+        self._erra = None
+        self._errkw = None
+
+    def setErrback(self, func, *a, **kw):
+        self._errfunc = func
+        self._erra = a
+        self._errkw = kw
+
+    def schedule(self, delay=0.0):
+        if self._call is None:
+            d = Deferred()
+            d.addCallback(self)
+            if self._errfunc:
+                d.addErrback(self.error)
+            self._call = reactor.callLater(delay, d.callback, None)
+
+    def cancel(self):
+        if self._call:
+            self._call.cancel()
+
+    def __call__(self, *args, **kwargs):
+        self._call = None
+        return self._func(*self._a, **self._kw)
+
+    def error(self, f):
+        self._call = None
+        if self._errfunc:
+            return self._errfunc(f, *self._erra, **self._errkw)
+        return f
