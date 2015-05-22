e157540d01db7ab1233f6edba938ff6c8ff757ab
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-05-22 20:08:49 +0200

    Test case for Scrapy+Frontera+MySpider suite.

---
+++ frontera/tests/scrapy_spider/frontera/settings.py
@@ -0,0 +1,14 @@
+#--------------------------------------------------------
+# Frontier
+#--------------------------------------------------------
+BACKEND = 'frontera.contrib.backends.memory.FIFO'
+MAX_REQUESTS = 5
+MAX_NEXT_REQUESTS = 1
+
+#--------------------------------------------------------
+# Logging
+#--------------------------------------------------------
+LOGGING_EVENTS_ENABLED = False
+LOGGING_MANAGER_ENABLED = False
+LOGGING_BACKEND_ENABLED = False
+LOGGING_DEBUGGING_ENABLED = False
