198a65602b5503c11c8bfdbe3e99366413248bd0
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-05-28 18:56:18 +0200

    Hostname local URL fingerprint.

--- crawlfrontier/utils/fingerprint.py
+++ crawlfrontier/utils/fingerprint.py
@@ -1,4 +1,7 @@
 import hashlib
+from urlparse import urlparse
+from zlib import crc32
+from struct import pack
 
 
 def sha1(key):
@@ -7,3 +10,15 @@ def sha1(key):
 
 def md5(key):
     return hashlib.md5(key.encode('utf8')).hexdigest()
+
+
+def hostname_local_fingerprint(key):
+    result = urlparse(key)
+    host_checksum = crc32(result.hostname) if type(result.hostname) is str else \
+        crc32(result.hostname.encode('utf-8', 'ignore'))
+    doc_uri_combined = result.path+';'+result.params+result.query+result.fragment
+
+    doc_uri_combined = doc_uri_combined if type(doc_uri_combined) is str else \
+        doc_uri_combined.encode('utf-8', 'ignore')
+    doc_fprint = hashlib.md5(doc_uri_combined).digest()
+    return pack(">i16B", host_checksum, doc_fprint)
