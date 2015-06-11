fd5f8b9fcf12e1ce9f512e8a1222ae0edf23e5c3
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-06-11 13:01:31 +0000

    Fix of kafka-python crash, by using partitioner class field, instead of argument.

--- crawlfrontier/worker/partitioner.py
+++ crawlfrontier/worker/partitioner.py
@@ -7,7 +7,7 @@ from kafka.partitioner.base import Partitioner
 
 class FingerprintPartitioner(Partitioner):
     def partition(self, key, partitions):
-        size = len(partitions)
+        size = len(self.partitions)
         digest = unhexlify(key[0:2] + key[5:7] + key[10:12] + key[15:17])
         value = unpack("<I", digest)
         idx = value[0] % size
@@ -20,6 +20,6 @@ class Crc32NamePartitioner(Partitioner):
         return self.partition_by_hash(value, partitions)
 
     def partition_by_hash(self, value, partitions):
-        size = len(partitions)
+        size = len(self.partitions)
         idx = value % size
         return partitions[idx]
