e9f892804cc18803b2685b637fab5f78be89218e
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-12-15 17:44:44 +0100

    DistributedBackend interface and SQLA implementation draft.

--- frontera/contrib/backends/sqlalchemy/__init__.py
+++ frontera/contrib/backends/sqlalchemy/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from frontera.core.components import DistributedBackend
 from frontera.contrib.backends import CommonBackend
 from frontera.contrib.backends.sqlalchemy.components import Metadata, Queue, States
 from frontera.contrib.backends.sqlalchemy.models import DeclarativeBase
@@ -101,3 +102,62 @@ LIFO = LIFOBackend
 FIFO = FIFOBackend
 DFS = DFSBackend
 BFS = BFSBackend
+
+
+class Distributed(DistributedBackend):
+    def __init__(self, manager):
+        self.manager = manager
+        settings = manager.settings
+        engine = settings.get('SQLALCHEMYBACKEND_ENGINE')
+        engine_echo = settings.get('SQLALCHEMYBACKEND_ENGINE_ECHO')
+        models = settings.get('SQLALCHEMYBACKEND_MODELS')
+        self.engine = create_engine(engine, echo=engine_echo)
+        self.models = dict([(name, load_object(klass)) for name, klass in models.items()])
+        self.session_cls = sessionmaker()
+        self.session_cls.configure(bind=self.engine)
+
+    @classmethod
+    def strategy_worker(cls, manager):
+        b = cls(manager)
+        settings = manager.settings
+        drop_all_tables = settings.get('SQLALCHEMYBACKEND_DROP_ALL_TABLES')
+        clear_content = settings.get('SQLALCHEMYBACKEND_CLEAR_CONTENT')
+        model = b.models['StateModel']
+
+        if drop_all_tables:
+            model.__table__.drop(bind=b.engine)
+        model.__table__.create(bind=b.engine)
+
+        if clear_content:
+            session = b.session_cls()
+            session.execute(model.__table__.delete())
+            session.close()
+        b._states = States(b.session_cls, model,
+                           settings.get('STATE_CACHE_SIZE_LIMIT'))
+        return b
+
+    @classmethod
+    def db_worker(cls, manager):
+        b = cls(manager)
+        settings = manager.settings
+        drop = settings.get('SQLALCHEMYBACKEND_DROP_ALL_TABLES')
+        clear_content = settings.get('SQLALCHEMYBACKEND_CLEAR_CONTENT')
+
+        metadata_m = b.models['MetadataModel']
+        queue_m = b.models['QueueModel']
+        if drop:
+            metadata_m.__table__.drop(bind=b.engine)
+            queue_m.__table__.drop(bind=b.engine)
+        metadata_m.__table__.create(bind=b.engine)
+        queue_m.__table__.create(bind=b.engine)
+
+        if clear_content:
+            session = b.session_cls()
+            session.execute(metadata_m.__table__.delete())
+            session.execute(queue_m.__table__.delete())
+            session.close()
+
+        b._metadata = Metadata(b.session_cls, metadata_m,
+                               settings.get('SQLALCHEMYBACKEND_CACHE_SIZE'))
+        b._queue = Queue(b.session_cls, queue_m, settings.get('SPIDER_FEED_PARTITIONS'))
+        return b
