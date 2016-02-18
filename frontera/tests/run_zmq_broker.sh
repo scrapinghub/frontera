f657f207170054f53f4478e43c96f2c747eb1333
Joris Rau <jrsr@lastline.com>
2016-02-18 17:27:23 +0000

    Do not kill!

--- frontera/tests/run_zmq_broker.sh
+++ frontera/tests/run_zmq_broker.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 # Start ZMQ broker with hostname instead of IP
 python -m frontera.contrib.messagebus.zeromq.broker --address localhost --port '5580' 2>> broker.log &
-kill $(ps aux | grep 'python -m frontera.contrib.messagebus.zeromq.broker' | awk '{print $2}')
 
 # Start ZMQ broker with IPv6
 python -m frontera.contrib.messagebus.zeromq.broker --address '::1' --port '5570' 2>> broker.log &
-kill $(ps aux | grep 'python -m frontera.contrib.messagebus.zeromq.broker' | awk '{print $2}')
 
 # Start ZMQ broker with wildcard
 python -m frontera.contrib.messagebus.zeromq.broker --address '*' --port '5560' 2>> broker.log &
-kill $(ps aux | grep 'python -m frontera.contrib.messagebus.zeromq.broker' | awk '{print $2}')
 
 # Start ZMQ broker with default settings
 python -m frontera.contrib.messagebus.zeromq.broker 2>> broker.log &
