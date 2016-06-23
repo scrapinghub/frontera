dbae8a2b1366b74365f95734f420a3ef59d259ea
Paul Tremberth <paul.tremberth@gmail.com>
2016-06-23 19:54:28 +0200

    Migrate backend tests

--- tests/test_revisiting_backend.py
+++ tests/test_revisiting_backend.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from frontera.tests.backends import BackendSequenceTest, TEST_SITES
+from tests.backends import BackendSequenceTest, TEST_SITES
 from frontera.utils.tester import FrontierTester
 
 from datetime import timedelta
