c4f9363cb191d0e42bc0bfd5a7dcbc17adcc18e1
Paul Tremberth <paul.tremberth@gmail.com>
2016-06-23 19:05:02 +0200

    [WIP] Move tests modules up in source tree

--- tests/test_scrapy_settings_adapter.py
+++ tests/test_scrapy_settings_adapter.py
@@ -10,6 +10,6 @@ def test_fallsback_to_crawler_settings():
 
 def test_frontera_settings_have_precedence_over_crawler_settings():
     crawler_settings = {'MAX_REQUESTS': 10,
-                        'FRONTERA_SETTINGS': 'frontera.tests.scrapy_spider.frontera.settings'}
+                        'FRONTERA_SETTINGS': 'tests.scrapy_spider.frontera.settings'}
     settings = ScrapySettingsAdapter(crawler_settings)
     assert settings.get('MAX_REQUESTS') == 5
