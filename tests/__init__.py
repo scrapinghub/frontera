f70ad38bf747660e3aa8d4cf8d49c239a57fc1c3
voith <voithjm1@gmail.com>
2016-09-27 22:05:34 +0530

    Added tests for S3SeedLoader

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
---
+++ tests/mocks/boto.py
@@ -0,0 +1,52 @@
+import six
+
+
+class Content(object):
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def split(self):
+        return self.obj
+
+
+class MockKey(object):
+
+    def __init__(self, name, data):
+        self.name = name
+        self.content = Content(data)
+
+    def get_contents_as_string(self, *args, **kwargs):
+        return self.content
+
+
+class MockBucket(object):
+
+    def __init__(self):
+        self.keys = {}
+
+    def list(self, prefix):
+        return [key for name, key in six.iteritems(self.keys) if name.startswith(prefix)]
+
+    def add_key(self, name, data):
+        if name in self.keys:
+            raise Exception('key: %s already exists' % name)
+        self.keys[name] = MockKey(name, data)
+
+
+class MockConnection(object):
+
+    def __init__(self):
+        self.buckets = {}
+
+    def get_bucket(self, bucket_name):
+        try:
+            return self.buckets[bucket_name]
+        except:
+            raise Exception('Bucket: %s not found' % bucket_name)
+
+    def create_bucket(self, name):
+        if name in self.buckets:
+            raise Exception('Bucket: %s already exists' % name)
+        self.buckets[name] = MockBucket()
+        return self.buckets[name]
