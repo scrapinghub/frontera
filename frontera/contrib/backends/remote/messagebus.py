6e44bf7134abb347899e83958dc66e0824faacd1
Anatolii Aniskovych <lin.aaa.lin@gmail.com>
2017-02-25 01:08:23 +0200

    Better code style for the message bus backend

--- frontera/contrib/backends/remote/messagebus.py
+++ frontera/contrib/backends/remote/messagebus.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+
+import logging
+from collections import defaultdict
+
+import six
+
 from frontera import Backend
 from frontera.core import OverusedBuffer
 from frontera.utils.misc import load_object
-import logging
-import six
 
 
 class MessageBusBackend(Backend):
@@ -21,8 +25,10 @@ class MessageBusBackend(Backend):
         self.spider_log_producer = self.mb.spider_log().producer()
         spider_feed = self.mb.spider_feed()
         self.partition_id = int(settings.get('SPIDER_PARTITION_ID'))
+
         if self.partition_id < 0 or self.partition_id >= settings.get('SPIDER_FEED_PARTITIONS'):
             raise ValueError("Spider partition id cannot be less than 0 or more than SPIDER_FEED_PARTITIONS.")
+
         self.consumer = spider_feed.consumer(partition_id=self.partition_id)
         self._get_timeout = float(settings.get('KAFKA_GET_TIMEOUT'))
         self._logger = logging.getLogger("messagebus-backend")
@@ -41,37 +47,46 @@ class MessageBusBackend(Backend):
         self.spider_log_producer.flush()
 
     def add_seeds(self, seeds):
-        per_host = aggregate_per_host(seeds)
-        for host_fprint, host_links in six.iteritems(per_host):
-            self.spider_log_producer.send(host_fprint,
-                                          self._encoder.encode_add_seeds(host_links))
+        for key, host_links in six.iteritems(aggregate_per_host(seeds)):
+            message = self._encoder.encode_add_seeds(host_links)
+
+            self.spider_log_producer.send(key, message)
 
     def page_crawled(self, response):
-        host_fprint = get_host_fprint(response)
-        self.spider_log_producer.send(host_fprint, self._encoder.encode_page_crawled(response))
+        key = get_host_fprint(response)
+        message = self._encoder.encode_page_crawled(response)
+
+        self.spider_log_producer.send(key, message)
 
     def links_extracted(self, request, links):
-        per_host = aggregate_per_host(links)
-        for host_fprint, host_links in six.iteritems(per_host):
-            self.spider_log_producer.send(host_fprint,
-                                          self._encoder.encode_links_extracted(request, host_links))
+        for key, host_links in six.iteritems(aggregate_per_host(links)):
+            message = self._encoder.encode_links_extracted(request, host_links)
+
+            self.spider_log_producer.send(key, message)
 
     def request_error(self, page, error):
-        host_fprint = get_host_fprint(page)
-        self.spider_log_producer.send(host_fprint, self._encoder.encode_request_error(page, error))
+        key = get_host_fprint(page)
+        message = self._encoder.encode_request_error(page, error)
+
+        self.spider_log_producer.send(key, message)
 
     def _get_next_requests(self, max_n_requests, **kwargs):
         requests = []
+
         for encoded in self.consumer.get_messages(count=max_n_requests, timeout=self._get_timeout):
             try:
                 request = self._decoder.decode_request(encoded)
-            except Exception as exc:
-                self._logger.warning("Could not decode message: {0}, error {1}".format(encoded, str(exc)))
+            except:
+                self._logger.exception("Could not decode message: %s", encoded)
             else:
                 requests.append(request)
-        self.spider_log_producer.send(b'0123456789abcdef0123456789abcdef012345678',
-                                      self._encoder.encode_offset(self.partition_id,
-                                                                  self.consumer.get_offset(self.partition_id)))
+
+        key = b'0123456789abcdef0123456789abcdef012345678'
+        offset = self.consumer.get_offset(self.partition_id)
+        message = self._encoder.encode_offset(self.partition_id, offset)
+
+        self.spider_log_producer.send(key, message)
+
         return requests
 
     def get_next_requests(self, max_n_requests, **kwargs):
@@ -94,18 +109,20 @@ class MessageBusBackend(Backend):
 
 
 def aggregate_per_host(requests):
-    per_host = dict()
-    for link in requests:
-        if b'fingerprint' not in link.meta[b'domain']:
+    per_host = defaultdict(list)
+
+    for request in requests:
+        try:
+            key = request.meta[b'domain'][b'fingerprint']
+            per_host[key].append(request)
+        except KeyError:
             continue
-        host_fprint = link.meta[b'domain'][b'fingerprint']
-        if host_fprint not in per_host:
-            per_host[host_fprint] = []
-        per_host[host_fprint].append(link)
+
     return per_host
 
 
 def get_host_fprint(request):
-    if b'fingerprint' not in request.meta[b'domain']:
+    try:
+        return request.meta[b'domain'][b'fingerprint']
+    except KeyError:
         return None
-    return request.meta[b'domain'][b'fingerprint']
