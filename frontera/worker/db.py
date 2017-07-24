ff6c7ddcf117768044c5ae7e7ce55a6fc7bc7c60
Viktor Shlapakov <vshlapakov@gmail.com>
2017-07-24 13:31:43 +0300

    Fix attribute error related with job_id

--- frontera/worker/db.py
+++ frontera/worker/db.py
@@ -110,6 +110,7 @@ class BaseDBWorker(object):
         self.slot = Slot(self, settings, **slot_kwargs)
 
         self.stats = defaultdict(int)
+        self.job_id = None
         self._logging_task = task.LoopingCall(self.log_status)
 
     def run(self):
