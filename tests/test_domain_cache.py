4e81956fe433b061218fbf36fc5496d29a5068a4
Alexander Sibiryakov <sixty-one@yandex.ru>
2018-05-11 13:24:03 +0200

    table creation

--- tests/test_domain_cache.py
+++ tests/test_domain_cache.py
@@ -9,6 +9,10 @@ class TestDomainCache(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(level=logging.DEBUG)
         self.conn = Connection(host="hbase-docker", table_prefix="contacts", table_prefix_separator=":")
+        if 'domain_metadata' not in self.conn.tables():
+            self.conn.create_table('domain_metadata', {
+                'm': {'max_versions': 1, 'block_cache_enabled': 1,}
+            })
         t = self.conn.table('domain_metadata')
         t.delete('d1')
         t.delete('d2')
