775dfbb5ccd63dcafd769bbdf8ac6301ab06dff5
Alexander Sibiryakov <sixty-one@yandex.ru>
2018-05-11 14:01:59 +0200

    crawling strategy test fix

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-from unittest import TestCase
-
-from frontera import FrontierManager
-from frontera.core.components import States
-from frontera.settings import Settings
 from frontera.worker.strategies import BaseCrawlingStrategy
 from frontera.worker.strategy import StatesContext
+from frontera.settings import Settings
+from frontera.core.manager import FrontierManager
+
+from frontera.contrib.backends.memory import MemoryStates
+from frontera.core.components import States
 
 
-class DummyCrawlingStrategy(BaseCrawlingStrategy):
+class TestingCrawlingStrategy(BaseCrawlingStrategy):
     def add_seeds(self, seeds):
         pass
 
@@ -21,6 +21,9 @@ class DummyCrawlingStrategy(BaseCrawlingStrategy):
     def links_extracted(self, request, links):
         pass
 
+    def filter_extracted_links(self, request, links):
+        pass
+
 
 class MessageBusStream(object):
     def send(self, request, score=1.0, dont_queue=False):
@@ -30,27 +33,30 @@ class MessageBusStream(object):
         pass
 
 
-class TestCrawlingStrategy(TestCase):
-    def setUp(self):
+class TestCrawlingStrategy(object):
+    def strategy(self):
         settings = Settings()
-        settings.BACKEND = "frontera.contrib.backends.memory.MemoryDistributedBackend"
-        self.manager = FrontierManager.from_settings(settings, db_worker=False, strategy_worker=True)
-        self.stream = MessageBusStream()
-        self.states_ctx = StatesContext(self.manager.backend.states)
-        self.strategy = DummyCrawlingStrategy(self.manager, self.stream, self.states_ctx)
+        settings.BACKEND = 'frontera.contrib.backends.sqlalchemy.Distributed'
+        manager = FrontierManager.from_settings(settings, db_worker=False, strategy_worker=True)
+        stream = MessageBusStream()
+        states = MemoryStates(10)
+        states_ctx = StatesContext(states)
+        return TestingCrawlingStrategy.from_worker(manager, stream, states_ctx)
 
     def test_create_request(self):
-        req = self.strategy.create_request("http://test.com/someurl")
+        s = self.strategy()
+        req = s.create_request("http://test.com/someurl")
         assert req.meta[b'fingerprint'] == b'955ac04f1b1a96de60a5139ad90c80be87822159'
 
     def test_states_refresh(self):
-        states = self.manager.backend.states
+        s = self.strategy()
+        states = s._states_context._states
         url = "http://test.com/someurl"
-        req1 = self.strategy.create_request(url)
+        req1 = s.create_request(url)
         req1.meta[b'state'] = States.CRAWLED
         states.update_cache(req1)
 
-        req2 = self.strategy.create_request(url)
-        self.strategy.refresh_states([req2])
+        req2 = s.create_request(url)
+        s.refresh_states([req2])
         assert req2.meta[b'state'] == req1.meta[b'state']
         assert req2.meta[b'state'] == States.CRAWLED
