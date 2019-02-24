ea8781b37138639060e97ee5f8497a400efcd50e
Lucy Wang <wxitb2017@gmail.com>
2019-02-24 23:14:28 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
