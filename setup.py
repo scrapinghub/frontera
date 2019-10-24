1f2684c74fb7813411b50c1c0ea81919081dc325
Susannah Klaneček <suzil@protonmail.ch>
2019-10-24 16:03:44 +0200

    Add thriftpy to requirements

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ setup(
     install_requires=[
         'six>=1.8.0',
         'w3lib>=1.15.0',
-        'cityhash>=0.1.7'
+        'cityhash>=0.1.7',
+        "thriftpy"
     ],
     extras_require={
         'sql': [
