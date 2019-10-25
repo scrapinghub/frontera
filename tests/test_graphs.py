269a1d2e80d4c011036a172a8268c87f0cb33465
Susannah Klaneček <suzil@protonmail.ch>
2019-10-25 23:54:18 +0200

    Cast tmpdir to a string

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -5,6 +5,6 @@ from frontera.utils.graphs.generate_diagrams import generate_diagrams
 
 def test_generate_diagrams(tmpdir):
     """Generate some example graph visualizations."""
-    generate_diagrams(outdir=tmpdir)
+    generate_diagrams(outdir=str(tmpdir))
     for idx in range(1, 10):
         assert os.path.isfile(tmpdir / "graph_0{}.png".format(idx))
